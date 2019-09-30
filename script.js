540f2786e8e78594235ea289d585136388dce6d9
iripoli <iripoli96@gmail.com>
2019-09-30 19:31:40 +0200

    Se añadio el console log para que nos de detalles cuando presionan el boton de expandir menu.

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ var mostrarMenu=document.querySelector('header');
 botonMenu.addEventListener('click', function(){
     mostrarMenu.classList.toggle('headerAmpliado');
 });
+ botonMenu.addEventListener('click', function(event){
+     console.log(event);
+ })
