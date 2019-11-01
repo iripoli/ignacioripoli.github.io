d4150bb2eccf95f3e7b8b2b5b43704d8a35706dd
iripoli <iripoli96@gmail.com>
2019-11-02 00:35:38 +0100

    Creacion de la version desktop, el formulario centrado, y nuevo menu para la version desktop

--- script.js
+++ script.js
@@ -2,9 +2,17 @@
 
 var botonMenu = document.querySelector('.nav__btn');
 var mostrarMenu=document.querySelector('header');
+var menuDorsal=document.querySelector('.header__nav')
+var tamañoPantalla=window.innerWidth;
+var abrirMenu=document.querySelector('header__nav');
+
+// Abrir el menu en pantallas menores a 1200px de ancho
 
 botonMenu.addEventListener('click', function(){
-    mostrarMenu.classList.toggle('headerAmpliado');
+    if (tamañoPantalla < 1000){
+    mostrarMenu.classList.toggle('headerAmpliado');}
+    else {menuDorsal.classList.toggle('nav__open');
+    botonMenu.classList.toggle('nav__btnOpen')};
 });
  botonMenu.addEventListener('click', function(event){
      console.log(event);
@@ -35,3 +43,11 @@ function myFunction(x) {
   function validateName(nombre){
       return nombre.length>4 ? true : false
   }
+
+
+// Abrir el menu desde el costado
+
+
+// botonMenu.addEventListener('click', function(){
+    
+// });
